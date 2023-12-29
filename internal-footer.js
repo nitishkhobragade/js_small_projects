4bc21c7caabb4e6b41f5393d53217200f0224a40
nitishkhobragade <nitishkhobragade89@gmail.com>
2023-12-29 19:47:19 +0530

    added website

---
+++ internal-footer.js
@@ -0,0 +1,31 @@
+
+document.getElementById('footer').innerHTML =
+`<div class="flex items-center justify-center" style="background-color: rgb(61, 66, 70);">
+<div class="m-2 max-w-5xl items-center">
+    <div class="flex flex-col items-center"> <img class="flex m-1" src="" />
+        <div class="m-2"> <a href="https://in.linkedin.com/in/nitishkhobragade"
+                class="w-9 h-10 text-center border-current border-2 text-violet-600 hover:text-black pt-1 inline-block"><i
+                    class="fa fa-linkedin m-2"></i></a> <a href="https://twitter.com/djnitish97"
+                class="w-9 h-10 text-center border-current border-2 text-blue-600 hover:text-black pt-1 inline-block"><i
+                    class="fa fa-twitter m-2"></i></a> <a href="
+                    https://github.com/nitishkhobragade"
+                class="w-9 h-10 text-center border-current border-2 text-rose-600 hover:text-black pt-1 inline-block"><i
+                    class="fa fa-github m-2"></i></a> <a href="#"
+                class="w-9 h-10 text-center border-current border-2 text-red-600 hover:text-black pt-1 inline-block"><i
+                    class="fa fa-pinterest m-2"></i></a> <a href="https://www.instagram.com/nitish_khobragade/"
+                class="w-9 h-10 text-center border-current border-2 text-orange-600 hover:text-black pt-1 inline-block"><i
+                    class="fa fa-instagram m-2"></i></a> </div>
+        <div class="flex md:flex-row flex-col">
+            <p class="hover:text-blue-600 text-gray-500 font-medium m-2"> <a href="../index.html">Home</a> </p>
+            <p class="hover:text-blue-600 text-gray-500 font-medium m-2"> <a href="../index.html">About us </a> </p>
+            <p class="hover:text-blue-600 text-gray-500 font-medium m-2"> <a href="#">Services </a> </p>
+            <p class="hover:text-blue-600 text-gray-500 font-medium m-2"> <a href="#">Contact Us </a> </p>
+            <p class="hover:text-blue-600 text-gray-500 font-medium m-2"> <a href="#"> Privacy Policy </a> </p>
+        </div>
+        <div class="text-gray-500 font-medium mt-4 text-xs"> Copyright 2023. Made by <a href="#"
+                class="text-blue-600">Nitish Khobragade</a> </div>
+    </div>
+</div>
+</div>
+<script src="https://use.fontawesome.com/03f8a0ebd4.js"></script>
+<script src="https://cdn.tailwindcss.com"></script>`
---
+++ internal-header.js
@@ -0,0 +1,50 @@
+
+document.getElementById('header').innerHTML =
+`<div>
+<div class="flex justify-center ">
+    <nav class="self-center fixed top-0 w-full max-w-7xl " style="background-color: rgb(61, 66, 70);">
+        <div class="flex justify-between items-center text-white">
+            <h1 class="uppercase pl-5 py-4 text-lg font-sans font-bold">tailwind</h1>
+            <ul class="hidden md:flex items-center text-[18px] font-semibold pl-32">
+                <li class="hover:underline underline-offset-4 decoration-2 decoration-white py-2 rounded-lg px-5"><a
+                        href="#">Home</a></li>
+                <li class="hover:underline underline-offset-4 decoration-2 decoration-white py-2 rounded-lg px-5"><a
+                        href="#">Contact</a></li>
+                <li class="hover:underline underline-offset-4 decoration-2 decoration-white py-2 rounded-lg px-5"><a
+                        href="#">Services</a></li>
+                <li class="hover:underline underline-offset-4 decoration-2 decoration-white py-2 rounded-lg px-5"><a
+                        href="#">About</a></li>
+                <li class="hover:underline underline-offset-4 decoration-2 decoration-white py-2 rounded-lg px-5"><a
+                        href="#">Pricing</a></li>
+            </ul>
+            <div class="text-white text-center text-base pr-5 hidden lg:inline-flex"> <a href="https://twitter.com/djnitish97"
+                    class="w-8 h-8 inline-block rounded-full pt-[6px] hover:text-blue-500"><i
+                        class="fa fa-twitter"></i></a> <a href="https://www.instagram.com/nitish_khobragade/"
+                    class="w-8 h-8 inline-block rounded-full pt-[5px] hover:text-blue-500"><i
+                        class="fa fa-instagram"></i></a> <a href="#"
+                    class="w-8 h-8 inline-block rounded-full pt-[5px] hover:text-blue-500"><i
+                        class="fa fa-facebook"></i></a> <a href="https://github.com/nitishkhobragade"
+                    class="w-8 h-8 inline-block rounded-full pt-[5px] hover:text-blue-500"><i
+                        class="fa fa-github"></i></a> <a href="https://in.linkedin.com/in/nitishkhobragade"
+                    class="w-8 h-8 inline-block rounded-full pt-[5px] hover:text-blue-500"><i
+                        class="fa fa-linkedin"></i></a> </div> <button
+                class="block p-3 mx-10 md:hidden hover:bg-black rounded-xl group">
+                <div class="w-5 my-[3px] h-[3px] bg-white mb-[2px]"></div>
+                <div class="w-5 my-[3px] h-[3px] bg-white mb-[2px]"></div>
+                <div class="w-5 my-[3px] h-[3px] bg-white"></div>
+                <div
+                    class="absolute top-0 -right-full opacity-0 h-screen w-[60%] border bg-black group-focus:right-0 group-focus:opacity-100 transition-all ease-in duration-300 " style="background-color: rgb(61, 66, 70);">
+                    <ul class="flex flex-col items-center text-[18px] pt-[60px]">
+                        <li class="hover:border border-white py-4 px-6 w-full"><a href="../index.html">Home</a></li>
+                        <li class="hover:border border-white py-4 px-6 w-full"><a href="../index.html">Contacts</a></li>
+                        <li class="hover:border border-white py-4 px-6 w-full"><a href="#">Services</a></li>
+                        <li class="hover:border border-white py-4 px-6 w-full"><a href="../index.html">About</a></li>
+                        <li class="hover:border border-white py-4 px-6 w-full"><a href="#">Pricing</a></li>
+                    </ul>
+                </div>
+            </button>
+        </div>
+    </nav>
+    </div>
+    <script src="https://use.fontawesome.com/03f8a0ebd4.js"></script> 
+</div>`
