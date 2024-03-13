25147c11bec629212ca2b2f4b190284534950f51
nitishkhobragade <nitishkhobragade89@gmail.com>
2024-03-13 07:14:27 +0530

    16_filter_and_search_01 fully coded

--- 16_filter_and_search_01/app.js
+++ 16_filter_and_search_01/app.js
@@ -54,8 +54,8 @@ let products = {
 for (let i of products.data) {
     ////Creating card
     let card = document.createElement("div");
-    ////Card should have category
-    card.classList.add("card", i.category);
+    ////Card should have category and stay hidden initially
+    card.classList.add("card", i.category, "hide");
     ////image div
     let imgContainer = document.createElement("div");
     imgContainer.classList.add("image-container");
@@ -80,3 +80,61 @@ for (let i of products.data) {
     card.appendChild(container);
     document.getElementById("products").appendChild(card);
 }
+
+////parameter passed from button (parameter same as category)
+function filterProduct(value) {
+    //Button class code
+    let buttons = document.querySelectorAll(".button-value");
+    buttons.forEach((button) => {
+        ////check if value equals to innerText
+        if (value.toUpperCase() == button.innerText.toUpperCase()) {
+            button.classList.add("active");
+        } else {
+            button.classList.remove("active");
+        }
+    });
+
+    ////select all cards
+    let elements = document.querySelectorAll(".card");
+    ////loop through all cards
+    elements.forEach((element) => {
+        //display all cards on all button click
+        if(value == "all") {
+            element.classList.remove("hide");
+        } else{
+            // check if element contains category 
+            if(element.classList.contains(value)) {
+                //display element based on category
+                element.classList.remove("hide");
+            } else {
+                ////hide other elements
+                element.classList.add("hide");
+            }
+        }
+    });
+};
+
+//search button click
+document.getElementById("search").addEventListener('click', () => {
+    //initializations
+    let searchInput = document.getElementById("search-input").value;
+    let elements = document.querySelectorAll(".product-name");
+    let cards = document.querySelectorAll(".card");
+    
+    ////loop through all elements
+    elements.forEach((element, index) => {
+        ////check if text includesvsearch value
+        if(element.innerText.includes(searchInput.toUpperCase())) {
+            ////displaying matching card
+            cards[index].classList.remove("hide");
+        } else {
+            ////hide others
+            cards[index].classList.add("hide");
+        }
+    });
+});
+
+////To initially display all products 
+window.onload = () => {
+    filterProduct("all");
+};
